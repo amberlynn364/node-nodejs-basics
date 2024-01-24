65d86b42ea1b65b7f5214cd5ecf17d12eaca7449
amberlynn <amberlynn364@gmail.com>
2024-01-24 20:10:33 +0300

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import { writeFile, access, constants } from 'fs/promises';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+	const filePath = resolve(__dirname, 'files', 'fresh.txt')
+	try {
+		await access(filePath, constants.F_OK);
+		throw new Error('FS operation failed');
+	} catch (error) {
+		if (error.code === 'ENOENT') {
+			try {
+				await writeFile(filePath, 'I am fresh and young')
+				console.log('File successfully created.');
+			} catch (error) {
+				console.error('Error creating file:', error);
+			}
+		} else {
+			throw error;
+		}
+	}
 };
 
-await create();
+create();
