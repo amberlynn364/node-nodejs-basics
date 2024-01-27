3b04012b2a937021decd21ec20a890cb91c38b81
amberlynn <amberlynn364@gmail.com>
2024-01-27 11:33:26 +0300

    feat: implement read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,25 @@
+import { createReadStream } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fileToRead = join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const readableStream = createReadStream(fileToRead);
+
+    readableStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    })
+
+    readableStream.on('end', () => {
+        console.log('\nFile has been read')
+    })
+
+    readableStream.on('error', (err) => {
+        console.error(`Error reading the file: ${err}`);
+    })
 };
 
 await read();
