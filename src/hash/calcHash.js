8295d3cd6a3ada32a62cf1dbf9a92aad4583ebe8
amberlynn <amberlynn364@gmail.com>
2024-01-27 11:26:15 +0300

    feat: implement calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,7 +1,24 @@
 import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fileToRead = join(__dirname, 'files', 'fileToCalculateHashFor.txt')
 
 const calculateHash = async () => {
     const hash = createHash('sha256');
+    const input = createReadStream(fileToRead);
+
+    input.on('readable', () => {
+        const data = input.read();
+        data ? hash.update(data) : console.log(hash.digest('hex'));
+    })
+
+    input.on('error', (err) => {
+        console.error(err);
+    })
 };
 
 await calculateHash();
