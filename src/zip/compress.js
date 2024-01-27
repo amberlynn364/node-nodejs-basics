60810d78ac85269bb51b7d9826d1c5fcd0c522e5
amberlynn <amberlynn364@gmail.com>
2024-01-27 13:34:58 +0300

    feat: implement copmress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,27 @@
+import { createWriteStream, createReadStream } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { createGzip } from 'zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fileToCompress = join(__dirname, 'files', 'fileToCompress.txt');
+const compressedFile = join(__dirname, 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip();
+    const input = createReadStream(fileToCompress);
+    const output = createWriteStream(compressedFile);
+
+    input.pipe(gzip).pipe(output);
+
+    output.on('finish', () => {
+        console.log('File has been compressed to archive.gz');
+    })
+
+    output.on('error', (err) => {
+        console.log(`'Error compressing file: ${err}`);
+    })
 };
 
 await compress();
