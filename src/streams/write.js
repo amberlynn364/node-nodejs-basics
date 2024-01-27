96dbeb5d416c4fb50a54deb71a038dcfd29d48c4
amberlynn <amberlynn364@gmail.com>
2024-01-27 13:18:33 +0300

    feat: implement write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import { createWriteStream } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fileToWrite = join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const writableStream = createWriteStream(fileToWrite);
+
+    process.stdin.pipe(writableStream);
+
+    process.stdin.on('end', () => {
+        process.stdin.destroy();
+    })
+
+    process.stdin.on('error', (err) => {
+        console.error(`Error writing data to file: ${err}`);
+    })
 };
 
 await write();
